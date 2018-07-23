2d1fb7965f2b6b9c127f712ca56eb030954c2d76
technologyplusplus <38882618+technologyplusplus@users.noreply.github.com>
2018-07-23 12:28:03 -0400

    Added UseUrls

--- WebServices/Program.cs
+++ WebServices/Program.cs
@@ -18,11 +18,12 @@ namespace WebServices
             BuildWebHost(args).Run();
         }
 
-        
+
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")
                 .Build();
     }
 }
